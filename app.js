9d5e4609ef7a99fde48e2e30c81c4f89856d1deb
Aymen <a.sakouhi@reply.de>
2023-01-25 14:36:13 +0100

    main commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const appFirstMain = "Main";
 const addingOneMoreUpdateOnMain = "new update";
 
-const whatever = 'bla bla bla';
-const whatever2 = 'bla bla bla 2';
+const whatever = "bla bla bla";
+const whatever2 = "bla bla bla 2";
+
+const appOnFeatureNow = "This one is from feature";
