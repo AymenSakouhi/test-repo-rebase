95c46aeca7f5e21102007df54dea061e4440182b
Aymen Sakouhi <37510225+AymenSakouhi@users.noreply.github.com>
2023-01-25 14:46:03 +0100

    some new change

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ const appOnFeatureNow = "This one is from feature";
 
 const featureWhatever = "bla bla";
 const featureWhatever2 = "asdasdadasdas";
-
 const lineA = "13";
 const lineB = "14";
-
-
 const lineC = "16";
 const lineD = "17";
